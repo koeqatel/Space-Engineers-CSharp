aa9edc3093cf0bf466a175852ba23f021f399501
Sander <sander.nieuwenhuisen@gmail.com>
2022-07-23 23:50:03 +0200

    Added a coords showing tool, show coords on the flight seat.

--- SpaceEngineers/Coords.cs
+++ SpaceEngineers/Coords.cs
@@ -27,53 +27,13 @@ namespace SpaceEngineers.UWBlockPrograms.BatteryMonitor
         public void Main(string args)
         {
             Vector3D vCoords = Me.GetPosition();
-            string sCoords = "(" + vCoords.X + ", " + vCoords.Y + ", " + vCoords.Z + ")";
+            string sCoords = "(" + Math.Round(vCoords.X, 3) + ", " + Math.Round(vCoords.Y, 3) + ", " + Math.Round(vCoords.Z, 3) + ")";
 
             IMyCockpit oFlightSeat = GridTerminalSystem.GetBlockWithName("Cockpit: Flight Seat") as IMyCockpit;
-            oFlightSeat.SetValue<Int64>("Content", 1);
-            oFlightSeat.SetValue<Color>("FontColor", new Color(100, 100, 100));
 
-            this.Log(oFlightSeat);
+            IMyTextSurface oFlightSeatScreen = oFlightSeat.GetSurface(0);
 
-            oFlightSeat.SetValue<string>("Text", sCoords);
-        }
-
-        void Log(string sMessage)
-        {
-            IMyTextPanel oScreen = PreparePanel();
-
-            List<string> aOldText = oScreen.GetText().Split('\n').ToList();
-
-            if (aOldText.Count >= 17)
-                aOldText.RemoveAt(0);
-
-            string sNewText = String.Join(Environment.NewLine, aOldText.ToArray()) + Environment.NewLine + DateTime.Now.ToString("H:mm:ss") + ": " + sMessage;
-            oScreen.WriteText(sNewText);
-        }
-
-        void Log(IMyTerminalBlock aMessage)
-        {
-            IMyTextPanel oScreen = PreparePanel();
-
-            //Get Actions
-            List<ITerminalAction> aActions = new List<ITerminalAction>();
-            aMessage.GetActions(aActions);
-
-            string sActions = "Actions: " + Environment.NewLine;
-
-            for (int i = 0; i < aActions.Count; i++)
-                sActions += aActions[i].Name + Environment.NewLine;
-
-            //Get Properties
-            List<ITerminalProperty> aProperties = new List<ITerminalProperty>();
-            aMessage.GetProperties(aProperties);
-
-            string sProperties = "Properties: " + Environment.NewLine;
-
-            for (int i = 0; i < aProperties.Count; i++)
-                sProperties += aProperties[i].Id + Environment.NewLine;
-
-            oScreen.WriteText(sActions + Environment.NewLine + sProperties);
+            oFlightSeatScreen.WriteText(sCoords);
         }
 
         #region PreludeFooter
