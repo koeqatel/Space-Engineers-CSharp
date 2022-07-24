f4af9eeb5e7037682988da142d9fe115cd0adc0d
Sander <sander.nieuwenhuisen@gmail.com>
2022-07-24 02:32:51 +0200

    Added a little color display with the currently known colors

---
+++ SpaceEngineers/Colors.cs
@@ -0,0 +1,54 @@
+public void Main(string argument, UpdateType updateSource)
+{
+    IMyTextPanel oScreen = GridTerminalSystem.GetBlockWithName("[Battery Status LCD]") as IMyTextPanel;
+
+    string sRainbow = ""; // Grays
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+
+    sRainbow += Environment.NewLine; // Reds
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+
+    sRainbow += Environment.NewLine; // Yellows
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+
+    sRainbow += Environment.NewLine; // Greens
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+
+    sRainbow += Environment.NewLine; // Cyans
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+    sRainbow += "";
+
+
+    sRainbow += Environment.NewLine; // Clean
+    sRainbow += "";
+
+
+    sRainbow += Environment.NewLine;
+
+    oScreen.WriteText(sRainbow);
+}
