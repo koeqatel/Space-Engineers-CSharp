ce7811fdd417b6db3f4a3a634d2c2792c5907726
Sander <sander.nieuwenhuisen@gmail.com>
2022-07-30 21:02:46 +0200

    Coords now completely sets up the lcd by itself

--- SpaceEngineers/Coords.cs
+++ SpaceEngineers/Coords.cs
@@ -1,45 +1,25 @@
-#region Prelude
-using System;
-using System.Linq;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
 
-using VRageMath;
-using VRage.Game;
-using VRage.Collections;
-using Sandbox.ModAPI.Ingame;
-using VRage.Game.Components;
-using VRage.Game.ModAPI.Ingame;
-using Sandbox.ModAPI.Interfaces;
-using Sandbox.Game.EntityComponents;
-using SpaceEngineers.Game.ModAPI.Ingame;
-using VRage.Game.ObjectBuilders.Definitions;
-
-// Change this namespace for each script you create.
-namespace SpaceEngineers.UWBlockPrograms.BatteryMonitor
+public Program()
 {
-    public sealed class Program : MyGridProgram
-    {
-        // Your code goes between the next #endregion and #region
-        #endregion
-
-        public Program()
-        {
-            Runtime.UpdateFrequency = UpdateFrequency.Update1;
-        }
-        public void Main(string args)
-        {
-            IMyCockpit oFlightSeat = GridTerminalSystem.GetBlockWithName("Cockpit: Flight Seat") as IMyCockpit;
-
-            Vector3D vCoords = oFlightSeat.GetPosition();
-            string sCoords = "(" + Math.Round(vCoords.X, 3) + ", " + Math.Round(vCoords.Y, 3) + ", " + Math.Round(vCoords.Z, 3) + ")";
-
-            IMyTextSurface oFlightSeatScreen = oFlightSeat.GetSurface(0);
-            oFlightSeatScreen.WriteText(sCoords);
-        }
-
-        #region PreludeFooter
-    }
+    Runtime.UpdateFrequency = UpdateFrequency.Update1;
 }
-#endregion
+public void Main(string args)
+{
+    IMyCockpit oFlightSeat = GridTerminalSystem.GetBlockWithName("Cockpit: Flight Seat") as IMyCockpit;
+
+    Vector3D vCoords = oFlightSeat.GetPosition();
+    string sCoords = "(" + Math.Round(vCoords.X, 3) + ", " + Math.Round(vCoords.Y, 3) + ", " + Math.Round(vCoords.Z, 3) + ")";
+
+    IMyTextSurface oFlightSeatScreen = oFlightSeat.GetSurface(0);
+
+    oFlightSeatScreen.ContentType = ContentType.TEXT_AND_IMAGE;
+    // Blue bg color = 0, 136, 190
+    oFlightSeatScreen.FontColor = new Color(255, 255, 255);
+    oFlightSeatScreen.BackgroundColor = new Color(0, 136, 190);
+    oFlightSeatScreen.Font = "Monospace";
+    oFlightSeatScreen.FontSize = 4;
+    oFlightSeatScreen.TextPadding = 32;
+    oFlightSeatScreen.Alignment = TextAlignment.CENTER;
+
+    oFlightSeatScreen.WriteText(sCoords);
+}
